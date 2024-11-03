efe1ec152c20be1c65109650dc23b40f7839f2ec
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-11-03 11:14:25 +0530

    recommendation system completed

---
+++ Movie Recommendation System/Web Application/App.py
@@ -0,0 +1,33 @@
+import pickle
+
+import streamlit as st
+
+st.title("Movie Recommender System")
+
+movies_df = pickle.load(open('movies.pkl', "rb"))
+movies_list = movies_df['title'].values
+
+similarity = pickle.load(open('similarity.pkl', "rb"))
+
+selected_movie_name = st.selectbox(
+    'Select a movie to get recommendations:',
+    movies_list
+)
+
+
+def recommend(movie):
+    movie_index = movies_df[movies_df['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+
+    for i in movie_list:
+        recommended_movies.append(movies_df.iloc[i[0]].title)
+    return recommended_movies
+
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie_name)
+    for rec in recommendations:
+        st.write(rec)
